26ecfb3cd667dab621b682aba041888814823e6a
Bonaeineza31 <b.ineza@alustudent.com>
2026-02-03 14:48:43 +0200

    Reduce the padding size to clearly see the overflow issue

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(18),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
